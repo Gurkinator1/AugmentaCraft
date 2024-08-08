604daec276c14ddb0262e8c606626d81fb574084
Florian Goerke <florian@goerke.de>
2024-08-08 22:53:20 +0200

    initial commit

---
+++ src/main/java/de/gurknator/AugmentaCraftDataGenerator.java
@@ -0,0 +1,11 @@
+package de.gurknator;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AugmentaCraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
